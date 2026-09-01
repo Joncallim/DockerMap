6ebfbd1a3b18a521d419667cee7489235e847f02
Jonathan <64296013+Joncallim@users.noreply.github.com>
2026-09-01 17:38:49 +0800

    refactor(daemon): extract npm provider

--- crates/dockermap-daemon/src/providers/mod.rs
+++ crates/dockermap-daemon/src/providers/mod.rs
@@ -1,6 +1,7 @@
 pub(crate) mod cron;
 pub(crate) mod listeners;
 pub(crate) mod network_infrastructure;
+pub(crate) mod npm;
 pub(crate) mod overlay_network;
 pub(crate) mod pm2;
 pub(crate) mod processes;
